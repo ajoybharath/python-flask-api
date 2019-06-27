5ccace66eea1fd1dd4c38369443a771df9c9b2db
Ajoy Bharath <ajoybharath@gmail.com>
2019-06-27 18:11:54 +0530

    modifiedapp file

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     runner = xmlrunner.XMLTestRunner(output='test-reports')
     unittest.main(testRunner=runner)
     ###########################################
-    unittest.main(
+    unittest.main()
